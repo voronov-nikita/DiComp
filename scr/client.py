407b52fc65aad2a5b456e8e5cbca89ff4716660e
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-05-30 17:57:54 +0300

    main-code-start

--- scr/client.py
+++ scr/client.py
@@ -3,3 +3,19 @@
 # Здесь же происходит отправка результатов вычислений обратно на сервер.
 
 
+# <============= Импорты библиотек =============>
+import socket
+import threading
+
+
+def accept_connect(ip:str, port:int):
+    while True:
+        sock = socket.socket()
+        sock.bind((ip, port))
+        sock.listen()
+        conn, addr = sock.accept()
+        print(conn, addr)
+
+
+k = accept_connect(socket.gethostbyname_ex(socket.gethostname())[-1][-1], 9999)
+print(k)
--- scr/server.py
+++ scr/server.py
@@ -3,3 +3,14 @@
 # на другие компьютеры. Здесь же происходит рапределение задач на подзадачи.
 
 
+# <============= Импорты библиотек =============>
+import socket
+import threading
+
+
+def create_connect(ip:str, port:int):
+    sock = socket.socket()
+    sock.connect((ip, port))
+
+    
+create_connect(socket.gethostbyname_ex(socket.gethostname())[-1][-1], 9999)
