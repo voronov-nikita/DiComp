c314bc20090fce20d81f77a2f60cda619d649e1b
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-06-06 22:22:50 +0300

    чет новое

--- scr/test-client.py
+++ scr/test-client.py
@@ -1,42 +1,13 @@
-import socket
-import subprocess
 
-def receive_file_and_execute(host, port):
-    # Создаем сокет
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Связываем сокет с адресом и портом
-    sock.bind((host, port))
+def fact(n):
+    if n==1:
+        return 1
+    return n*fact(n-1)
 
-    # Слушаем входящие соединения
-    sock.listen()
+def non(n):
+    if n%2==0:
+        return True
+    return False
 
-    # Принимаем соединение
-    conn, addr = sock.accept()
-
-    # Создаем временный файл для сохранения полученного файла
-    temp_file_path = 'new-new.py'
-
-    # Получаем данные файла
-    with open(temp_file_path, 'wb') as file:
-        while True:
-            data = conn.recv(4096)
-            if not data:
-                break
-            file.write(data)
-    result = subprocess.run(['python ', temp_file_path], capture_output=True, text=True)
-    print(result.stdout)
-    
-    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    send_socket.connect((host, port))
-    send_socket.sendall(result.stdout.encode())
-
-    # Закрываем соединение
-    conn.close()
-
-
-# Пример использования
-host = '0.0.0.0'  # Слушаем на всех доступных интерфейсах
-port = 12345
-
-receive_file_and_execute(host, port)
+def start():
+    return True
--- scr/test-function.py
+++ scr/test-function.py
@@ -1,11 +1,22 @@
-import requests
+import os
 
-def get_external_ip():
-    response = requests.get('https://api.ipify.org?format=json')
-    data = response.json()
-    external_ip = data['ip']
-    return external_ip
+with open("test-client.py", 'r') as file:
+    k=1
+    ls=[]
+    s=""
+    for line in file:
+        if "class" in line:
+            project_folder = os.getcwd()
+            p = os.path.join(project_folder, line[6:-2])
+            os.mkdir(p)
+        if "def" in line:
+            ls.append(s)
+            s=""
+        s+=line
+    ls.append(s)
+    del ls[0]
 
-# Пример использования
-external_ip = get_external_ip()
-print("Внешний IP-адрес:", external_ip)
+
+for k in range(len(ls)):
+    with open(f"task{k}.txt", "w") as file_write:
+        file_write.write(ls[k])
