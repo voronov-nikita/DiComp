7a5287eceab216625090752f6461589b68975957
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-06-13 22:39:42 +0300

    new_information

--- scr/client.py
+++ scr/client.py
@@ -1,21 +1,17 @@
-# Клиент
-# Получает файлы и зaдачи, которые должен обраюотать
-# Здесь же происходит отправка результатов вычислений обратно на сервер.
+# Это по факту тот, кто отправляет входные данные
+# И принимает ответ от "сервера" в виде результата функции
 
-
-# <============= Импорты библиотек =============>
 import socket
-import threading
 
+def send_request(request):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 9999))
+    client_socket.send(request.encode())  # Отправляем запрос на сервер
+    response = client_socket.recv(1024).decode()  # Получаем ответ от сервера
+    print("Результат вычислений:", response)
+    client_socket.send(response.encode('utf-8'))
+    client_socket.close()
 
-def accept_connect(ip:str, port:int):
-    while True:
-        sock = socket.socket()
-        sock.bind((ip, port))
-        sock.listen()
-        conn, addr = sock.accept()
-        print(conn, addr)
-
-
-k = accept_connect(socket.gethostbyname_ex(socket.gethostname())[-1][-1], 9999)
-print(k)
+if __name__ == '__main__':
+    request = input("Введите число для вычислений: ")
+    send_request(request)
--- scr/server.py
+++ scr/server.py
@@ -1,18 +1,37 @@
-# Сервер
-# Это та чать, которая отправляет задачи
-# на другие компьютеры. Здесь же происходит рапределение задач на подзадачи.
+# Это по факту тот, кто принимает на вход какие-либо данные
+# А потом выполняет определенную функцию с экземплярами этих данных
+# И возвращает обратно ответ этой функции
 
 
-# <============= Импорты библиотек =============>
 import socket
 import threading
 
+def handle_client(client_socket):
+    request = client_socket.recv(1024).decode()  # Получаем запрос от клиента
+    # Выполняем вычисления на сервере
+    result = perform_computation(request)
+    # Отправляем результат клиенту
+    client_socket.send(result.encode())
+    client_socket.close()
 
-def create_connect(ip:str, port:int):
-    sock = socket.socket()
-    sock.connect((ip, port))
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', 9999))
+    server_socket.listen(5)
+    print("Сервер запущен и ожидает подключений...")
 
-    
-class Boss:
-    def __init__(self):
-        print("OK")
+    while True:
+        client_socket, _ = server_socket.accept()
+        print("Получено подключение от клиента")
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+def perform_computation(request):
+    # Выполняем вычисления на сервере и возвращаем результат
+    # В этом примере просто удваиваем полученное число
+    number = int(request)
+    result = number * 2
+    return str(result)
+
+if __name__ == '__main__':
+    start_server()
