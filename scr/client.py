50e85c30b489fedb2f2c8165f267b4e8141f3e4f
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-06-19 23:40:59 +0300

    what is it

--- scr/client.py
+++ scr/client.py
@@ -4,24 +4,29 @@
 
 import inspect
 import socket
+import os
+
 
 COUNT_TASKS:int = 0
 LEN_NAME_DECORATOR:int = len("@save_send_file ")
 
+IP:str = "localhost"
+PORT:int = 12345
+
 
 def save_send_file(func):
-    # аодготовка файлов для сервера
+    # подготовка файлов для сервера
     def wrapper(*args, **kwargs):
         global COUNT_TASKS
         # Получение исходного кода функции
         source_code = inspect.getsource(func)
 
-        # запись 
+        # запись задачи
         with open(f"task{COUNT_TASKS}.txt", "w") as file:
             file.write(source_code[LEN_NAME_DECORATOR:])
         
-
-        with open(f"value{COUNT_TASKS}", "w") as file:
+        # соответсвующие значения для функций
+        with open(f"value{COUNT_TASKS}.txt", "w") as file:
             for elem in args:
                 file.write(str(elem))
                 file.write("\n")
@@ -30,13 +35,26 @@ def save_send_file(func):
 
 
     # отправка файла на сервер
-    def send_file(file_name:str, ip:str, port:int):
+    def send_file(file_name:str):
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            client_socket.connect((ip, port))
+            client_socket.connect((IP, PORT))
+            file = open(file_name, 'rb')
+
+            file_line = file.read(1024)
+
+            while file_line:
+                client_socket.send(file_line)
+                file_line = file.read(1024)
+
+            file.close()
+            os.remove(f"/{file_name}")
+            os.remove(f"/{file_name}")
+                
 
         except:
             print("ERROR CONNECT")
+
 
     def get_result():
         return None
@@ -45,6 +63,12 @@ def save_send_file(func):
     # запуск всех состовляющих
     def run(*args, **kwargs):
         wrapper(*args, **kwargs)
+
+        for i in range(COUNT_TASKS):
+            send_file(f"task{i}.txt")
+            send_file(f"value{i}.txt")
+
+
         result = get_result()
         print(COUNT_TASKS)
         return result
--- scr/server.py
+++ scr/server.py
@@ -1,37 +1,43 @@
-# Это по факту тот, кто принимает на вход какие-либо данные
-# А потом выполняет определенную функцию с экземплярами этих данных
-# И возвращает обратно ответ этой функции
-
-
 import socket
-import threading
+import sys
 
-def handle_client(client_socket):
-    request = client_socket.recv(1024).decode()  # Получаем запрос от клиента
-    # Выполняем вычисления на сервере
-    result = perform_computation(request)
-    # Отправляем результат клиенту
-    client_socket.send(result.encode())
-    client_socket.close()
+# создаём сокет и связываем его с IP-адресом и портом
 
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 9999))
-    server_socket.listen(5)
-    print("Сервер запущен и ожидает подключений...")
+sock = socket.socket()
+ip = "localhost"
+port = 12345
+sock.bind((ip, port))
+
+# сервер ожидает передачи информации
+sock.listen(10)
+
+while True:
+    # начинаем принимать соединения
+    conn, addr = sock.accept()
+
+    # выводим информацию о подключении
+    print('connected:', addr)
+
+    # получаем название файла
+    name_f = (conn.recv(1024)).decode ('UTF-8')
+
+    # открываем файл в режиме байтовой записи в отдельной папке 'sent'
+    f = open(name_f,'wb')
 
     while True:
-        client_socket, _ = server_socket.accept()
-        print("Получено подключение от клиента")
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
 
-def perform_computation(request):
-    # Выполняем вычисления на сервере и возвращаем результат
-    # В этом примере просто удваиваем полученное число
-    number = int(request)
-    result = number * 2
-    return str(result)
+        # получаем байтовые строки
+        l = conn.recv(1024)
 
-if __name__ == '__main__':
-    start_server()
+        # пишем байтовые строки в файл на сервере
+        f.write(l)
+
+        if not l:
+            break
+
+    f.close()
+    conn.close()
+
+    print('File received')
+
+sock.close()
