92252df2229fcc6aad3b197cdc31939487ca8036
voronov <114691383+voronov-nikita@users.noreply.github.com>
2024-03-17 23:49:41 +0300

    add new files and gitignore

--- platform/service.py
+++ platform/service.py
@@ -8,13 +8,20 @@ HOST: str = "localhost"
 
 app = Flask(__name__)
 
-
+# соновной обработчик запросов (головной центр или домашняя старница)
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
 
+# обработчик ошибки 404 пользователя (не найден файл или каталог)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# обработчик принятия post запроса от кнопки с каким-то параметром
 @app.route('/handle_button_click', methods=['POST'])
 def handle_button_click():
     # Этот код будет выполняться при нажатии на кнопку
